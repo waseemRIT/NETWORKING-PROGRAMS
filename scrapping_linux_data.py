29e1f5bf8f0ee1dcd5cc0d57b3b2dfb7220c0794
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-09-20 14:48:28 +0400

    Default gateway notfound exception added

--- scrapping_linux_data.py
+++ scrapping_linux_data.py
@@ -5,8 +5,11 @@ def get_default_gateway():
     gateway = subprocess.run("ip route | grep default", shell=True, capture_output=True)
     # ip route | grep default
     ip_add = gateway.stdout.decode()
-    gateway_ip_add = ip_add.split()[2]
-    return gateway_ip_add
+    try:
+        gateway_ip_add = ip_add.split()[2]
+        return gateway_ip_add
+    except:
+        return "DEFAULT GATEWAY NOT FOUND"
 
 
 def ping_test(ip: str):
@@ -15,18 +18,21 @@ def ping_test(ip: str):
     stream = os.popen(f"ping -c4 {ip}")
     output = stream.read()
     if '0 received' in output:
-        print('IP unreachable')
+        return 'IP unreachable'
     else:
-        print('IP reachable')
+        return 'IP reachable'
     # print(output)
 
 
 def main():
     command = input("""Enter \"Gateway\" to test for Gateway connectivity.""")
     command.lower()
+
     if command == "gateway":
         gate_way_ip = get_default_gateway()
         print(ping_test(gate_way_ip))
+    else:
+        pass
 
 
 if __name__ == '__main__':
