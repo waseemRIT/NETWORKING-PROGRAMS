d925475f21fb2b9da260c3eecd32ee01b8418e29
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-09-20 17:54:32 +0400

    Update scrapping_linux_data.py

--- scrapping_linux_data.py
+++ scrapping_linux_data.py
@@ -23,9 +23,18 @@ def ping_test(ip: str):
         return 'IP reachable'
 
 
+def get_dns_ip(hostname: str):
+    dns_ip = subprocess.run(f"nslookup {hostname} | grep -i server: | cut -d\":\" -f2", shell=True, capture_output=True)
+
+    if "server can't find" in dns_ip:
+        return "DNS NOT FOUND"
+        dns_ip = subprocess.run(f"nslookup {hostname} | grep -i server: | cut -d\":\" -f2", shell=True, capture_output=True)
+    return dns_ip.stdout.decode().strip()
+
+
 def main():
     command = input("""Enter \"Gateway\" to test for Gateway connectivity.\nEnter \"it_dns\" to test for RIT DNS 
-    connectivity\nEnter google.com  to resolve to test validity""")
+    connectivity\nEnter \"dns_testing\" google.com  to resolve to test validity""")
     command.lower()
 
     if command == "gateway":
@@ -35,6 +44,8 @@ def main():
     elif command == "rit_dns":
         test_ip = "129.21.3.17"
         print(ping_test(test_ip))
+    elif command == "":
+        pass
 
 
 if __name__ == '__main__':
