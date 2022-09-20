ac39660ccbadfe88b7b485548bd6e8a1ec4d2c45
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-09-20 14:38:48 +0400

    Getting Gateway and Pinging it

---
+++ scrapping_linux_data.py
@@ -0,0 +1,34 @@
+import os, subprocess
+
+
+def get_default_gateway():
+    gateway = subprocess.run("ip route | grep default", shell=True, capture_output=True)
+    # ip route | grep default
+    ip_add = gateway.stdout.decode()
+    gateway_ip_add = ip_add.split()[2]
+    return gateway_ip_add
+
+
+def ping_test(ip: str):
+    # ping_result = subprocess.run(f"ping -c 4 {ip}", shell=True, capture_output=True)
+    # print(ping_result)
+    stream = os.popen(f"ping -c4 {ip}")
+    output = stream.read()
+    if '0 received' in output:
+        print('IP unreachable')
+    else:
+        print('IP reachable')
+    # print(output)
+
+
+def main():
+    command = input("""Enter \"Gateway\" to test for Gateway connectivity.""")
+    command.lower()
+    if command == "gateway":
+        gate_way_ip = get_default_gateway()
+        print(ping_test(gate_way_ip))
+
+
+if __name__ == '__main__':
+    # while True:
+    main()
