67b639de4249a5be8224c7b75cd6d50eb54cced0
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-09-24 01:50:28 +0400

    ALL WORKING MISSING DESIGN

--- scrapping_linux_data.py
+++ scrapping_linux_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 There are 3 Functions to this Program
 1. Testing the connectivity of the default gateway
@@ -11,6 +12,7 @@ There are 3 Functions to this Program
 
 import os  # Library Used For Pinging Utilities
 import subprocess  # Library used For Shell Commands Utilities
+import socket  # Library Used For DNS resolving utilities
 
 
 def get_default_gateway():
@@ -19,18 +21,19 @@ def get_default_gateway():
     returns
         the ip address of the default gateway
     or
-        DEFAULT GATEWAY NOT FOUND
+        returns 0
+        if default gateway is not available
     """
     # USED TO RUN THE SHELL COMMAND TO GET THE DEFAULT GATEWAY IP ADDRESS
-    gateway = subprocess.run("ip route | grep default", shell=True, capture_output=True)
-    # GETS THE OUTPUT OF THE SHELL COMMAND USED | DECODES THE IP ADDRESS FROM b to string
-    ip_add = gateway.stdout.decode()
+    gateway = subprocess.run(["sh", "-c", "ip route | grep default"], stdout=subprocess.PIPE)
+    # GETS THE OUTPUT OF THE SHELL COMMAND USED | DECODES THE IP ADDRESS FROM b (binaries) to string
+    ip_address = gateway.stdout.decode()
     try:  # TESTING IF AN IP ADDRESS WAS RETURNED
-        gateway_ip_add = ip_add.split()[2]  # STRIPING THE IP ADDRESS FROM THE REST OF THE INPUT
+        gateway_ip_add = ip_address.split()[2]  # STRIPING THE IP ADDRESS FROM THE REST OF THE INPUT
         return gateway_ip_add
     except IndexError:
         # IF THERE IS NO IP ADDRESS RETURNED, IT WON'T BE ABLE TO GET THE SECOND INDEX AFTER USING split()
-        return "DEFAULT GATEWAY NOT FOUND"
+        return 0
 
 
 def ping_test(ip: str):
@@ -39,12 +42,11 @@ def ping_test(ip: str):
     returns
         IP reachable if the connectivity worked
             only when all packets get no reply
-        IP unreachable if the connectivity didn't work
+        IP unreachable the connectivity didn't work
     """
     # USING os.system() TO RUN THE ping SHELL COMMAND
     output = os.system(f"ping -c4 {ip}")
-    # COMMAND TO GET THE OUTPUT OF THE ping COMMAND
-    # output = stream.read()
+
     # TESTING FOR ANY ERRORS
     if output == 0:
         return 'IP reachable'
@@ -54,41 +56,52 @@ def ping_test(ip: str):
 
 
 def get_dns_ip(hostname: str):
-    dns_ip = subprocess.run(f"nslookup {hostname}", shell=True, capture_output=True)
-    dns_ip_resolved = dns_ip.stdout.decode()
-    if "server can't find" in dns_ip_resolved:
-        return "DNS NOT FOUND"
-    else:
-        dns_ip = subprocess.run(f"nslookup {hostname} | grep -i server: | cut -d\":\" -f2", shell=True,
-                                capture_output=True)
-        return dns_ip.stdout.decode().strip()
+    try:
+        dns_ip = socket.gethostbyname(hostname)  # RESOLVES THE IP ADDRESS OF THE HOSTNAME
+        return dns_ip  # RETURNS THE IP ADDRESS
+    except:
+        return 0
 
 
 def exit_function():
+    """
+    FUNCTION TO EXIT THE PROGRAM
+    """
+    # os.system("exit")
     exit()
 
 
 def main():
     # CREATING A MENU FOR THE USER TO CHOOSE FROM THE FUNCTIONS OPTIONS
-    command = input("----------------------------------------------------------------------------------\n1.Enter "
-                    "\"gateway\" to test for Gateway connectivity.\n2.Enter \"rit_dns\" to test for RIT DNS "
-                    "connectivity\n3.Enter \"dns_testing\" google.com  to resolve to test validity\n----->")
-    command.lower()  # LOWERING THE INPUT OF THE USER JUST IN CASE
-    if command == "gateway":  # IF USER INPUT MATCHES gateway IT WILL TEST FOR THE GATEWAY FUNCTION
-        gate_way_ip = get_default_gateway()  # GETTING THE IP ADDRESS
-        print(f"The default Gateway: {gate_way_ip}")  # PRINTING THE DEFAULT GATEWAY
-        print(ping_test(gate_way_ip))  # PRINTING THE RESULT OF PINGING THE DEFAULT GATEWAY
+    command = input("----------------------------------------------------------------------------------\nEnter "
+                    "1 to test for Gateway connectivity.\nEnter 2 to test for RIT DNS "
+                    "connectivity\nEnter 3 google.com  to resolve to test validity\nEnter 4 to "
+                    "exit the program\n----->")
 
-    elif command == "rit_dns":  # IF THE USER INPUT MATCHES rit_dns IT WILL TEST FOR THE RIT DNS CONNECTIVITY
+    command.lower()  # LOWERING THE INPUT OF THE USER JUST IN CASE
+
+    if command == "1":  # IF USER INPUT MATCHES gateway IT WILL TEST FOR THE GATEWAY FUNCTION
+        gate_way_ip = get_default_gateway()  # GETTING THE IP ADDRESS
+        if gate_way_ip != 0:
+            print(f"The default Gateway: {gate_way_ip}")  # PRINTING THE DEFAULT GATEWAY
+            print(ping_test(gate_way_ip))  # PRINTING THE RESULT OF PINGING THE DEFAULT GATEWAY
+        else:
+            print("DEFAULT GATEWAY NOT FOUND")
+    elif command == "2":  # IF THE USER INPUT MATCHES rit_dns IT WILL TEST FOR THE RIT DNS CONNECTIVITY
         test_ip = "129.21.3.17"
         print(ping_test(test_ip))  # PRINTING THE RESULT OF PINGING THE RIT DNS
-
-    elif command == "dns_testing":  # IF THE USER INPUT MATCHES dns_testing IT WILL TEST FOR THE DNS/hostname
-        # CONNECTIVITY
+    elif command == "3":  # IF THE USER INPUT MATCHES dns_testing IT WILL TEST FOR THE DNS CONNECTIVITY
+        # GET THE HOSTNAME FROM THE USER
         hostname = input("What's the hostname: ")
-        dns_ip = get_dns_ip(hostname)
-        print(ping_test(dns_ip))  # PRINTING THE RESULT OF PINGING THE DNS/hostname
-    elif command == "exit":
+        if hostname != "exit_func":
+            # VARIABLE TO STORE THE VALUE OF THE  IP ADDRESS
+            dns_ip = get_dns_ip(hostname)
+            if dns_ip != 0:  # get_dns_ip returns 0 if an ERROR OCCURRED DURING RESOLVING HOSTNAME
+                print(ping_test(dns_ip))  # PRINTING THE RESULT OF PINGING THE DNS/hostname
+            else:  # SOMETHING WENT WRONG WITH RESOLVING THE IP ADDRESS
+                print(
+                    "DNS resolution NOT FOUND")
+    elif command == "4":
         print("---------------------------------------------------------------------")
         print("EXITING PING TEST")
         exit_function()
@@ -96,4 +109,8 @@ def main():
 
 if __name__ == '__main__':
     while True:  # KEEPS THE PROGRAM RUNNING TILL SOMEONE EXISTS USING CTRL + C
-        main()
+        try:
+            main()
+        except KeyboardInterrupt:
+            print('Force Exit Because of Keyboard Interruption')
+
